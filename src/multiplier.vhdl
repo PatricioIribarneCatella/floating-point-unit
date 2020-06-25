1613903e1cac0492b50eeda8a51c8e4d9cbf234f
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-06-25 18:59:27 -0300

    Simple multiplier coponent working

--- src/multiplier.vhdl
+++ src/multiplier.vhdl
@@ -9,13 +9,13 @@ entity multiplier is
 	port(
 		a : in std_logic_vector(mant_size downto 0);
 		b : in std_logic_vector(mant_size downto 0);
-		res : out std_logic_vector(2 * mant_size - 1 downto 0)
+		res : out std_logic_vector(2 * mant_size + 1 downto 0)
 	);
 end entity multiplier;
 
 architecture multiplier_arq of multiplier is
 begin
 	
-	res <= std_logic_vector(to_unsigned(to_integer(a) * to_integer(b), 2 * mant_size));
+	res <= std_logic_vector(unsigned(a) * unsigned(b));
 
 end architecture multiplier_arq;
