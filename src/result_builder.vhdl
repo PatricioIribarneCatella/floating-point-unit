8710d0a8bd9841a854f684b93bc0b25e1ee9c858
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-06-27 17:08:17 -0300

    Result Builder changed to return parts of the result instead of the complete result

--- src/result_builder.vhdl
+++ src/result_builder.vhdl
@@ -4,15 +4,16 @@ library IEEE;
 
 entity result_builder is
 	generic(
-		word_size: natural := 10;
 		exp_size: natural := 4;
 		mant_size : natural := 5
 	);
 	port(
 		sign : in std_logic;
-		mant : in std_logic_vector(mant_size - 1 downto 0);
-		exp : in integer;
-		res : out std_logic_vector(word_size - 1 downto 0)
+		mant_in : in std_logic_vector(mant_size - 1 downto 0);
+		exp_in : in integer;
+		sign_out : out std_logic;
+		mant_out : out std_logic_vector(mant_size - 1 downto 0);
+		exp_out : out integer
 	);
 end entity result_builder;
 
@@ -22,28 +23,31 @@ architecture result_builder_arq of result_builder is
 	constant MAX_EXP : integer := BIAS;
 	constant MIN_EXP : integer := -(BIAS - 1);
 
-	constant SAT_EXP : std_logic_vector(exp_size - 1 downto 0)
-						:= std_logic_vector(to_unsigned(MAX_EXP, exp_size));
+	constant SAT_EXP : integer := MAX_EXP;
 	constant SAT_MANT : std_logic_vector(mant_size - 1 downto 0)
 						:= (others => '1');
 
-	constant ZERO_EXP : std_logic_vector(exp_size - 1 downto 0)
-						:= (others => '0');
+	constant ZERO_EXP : integer := 0;
 	constant ZERO_MANT : std_logic_vector(mant_size - 1 downto 0)
 						:= (others => '0');
 begin
 
-	builder: process (sign, mant, exp)
+	builder: process (sign, mant_in, exp_in)
 	begin
-		if exp > MAX_EXP then
+		sign_out <= sign;
+
+		if exp_in > MAX_EXP then
 			-- saturation reached
-			res <= sign & SAT_EXP & SAT_MANT;
-		elsif exp < MIN_EXP then
+			exp_out <= SAT_EXP;
+			mant_out <= SAT_MANT;
+		elsif exp_in < MIN_EXP then
 			-- zero reached
-			res <= sign & ZERO_EXP & ZERO_MANT;
+			exp_out <= ZERO_EXP;
+			mant_out <= ZERO_MANT;
 		else
 			-- the result its OK
-			res <= sign & std_logic_vector(to_unsigned(exp + BIAS, exp_size)) & mant;
+			exp_out <= exp_in + BIAS;
+			mant_out <= mant_in;
 		end if;
 	end process;
 
--- testbench/result_builder_tb.vhdl
+++ testbench/result_builder_tb.vhdl
@@ -7,39 +7,40 @@ end entity result_builder_tb;
 
 architecture result_builder_tb_arq of result_builder_tb is
 
-	constant word_size_tb : natural := 10;
 	constant exp_size_tb : natural := 4;
 	constant mant_size_tb : natural := 5;
 
 	constant BIAS : natural := 2**(exp_size_tb - 1) - 1;
 
 	signal sign_aux : std_logic := '0';
-	signal mant_aux : std_logic_vector(mant_size_tb - 1 downto 0)
-						:= "10000";
+	signal exp_in_aux : integer := 4;
+	signal mant_in_aux : std_logic_vector(mant_size_tb - 1 downto 0)
+							:= "10000";
 
-	signal exp_aux : integer := 4;
-
-	signal res_aux : std_logic_vector(word_size_tb - 1 downto 0);
+	signal sign_out_aux : std_logic;
+	signal exp_out_aux : integer;
+	signal mant_out_aux : std_logic_vector(mant_size_tb - 1 downto 0);
 
 begin
 
 	sign_aux <= '1' after 100 ns, '0' after 200 ns, '1' after 300 ns;
-	exp_aux <= BIAS after 50 ns,
-			   (BIAS + 1) after 100 ns,
-			   -(BIAS - 1) after 150 ns,
-			   -(BIAS - 1) - 1 after 200 ns;
+	exp_in_aux <= BIAS after 50 ns, -- limit for max saturation
+				  -(BIAS - 1) after 100 ns, -- limit for min underflow
+				  (BIAS + 1) after 150 ns, -- limit for max saturation + 1
+				  -(BIAS - 1) - 1 after 200 ns; -- limit for min underflow - 1
 
 	DUT: entity work.result_builder
 		generic map(
-			word_size => word_size_tb,
 			exp_size => exp_size_tb,
 			mant_size => mant_size_tb
 		)
 		port map(
 			sign => sign_aux,
-			mant => mant_aux,
-			exp => exp_aux,
-			res => res_aux
+			exp_in => exp_in_aux,
+			mant_in => mant_in_aux,
+			sign_out => sign_out_aux,
+			exp_out => exp_out_aux,
+			mant_out => mant_out_aux
 		);
 
 end architecture result_builder_tb_arq;
