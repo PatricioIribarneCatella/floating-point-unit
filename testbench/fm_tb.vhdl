a30a9a7d7c2f5e3d6ea3f114dca8b734c9832d8b
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-06-30 15:06:47 -0300

    Add more info on error results

--- testbench/fm_tb.vhdl
+++ testbench/fm_tb.vhdl
@@ -9,22 +9,22 @@ architecture fm_tb_arq of fm_tb is
 
 	-- Errors:
 	--	File: fmul_15_6.txt
-	--		2534066 2064383 3609004
-	--		2064383 2618694 3674602
-	--		722076 2416840 2126960 -> resultado del exponente es -31
-	--		2064383 873228 1942471
+	--		A: 2534066 B: 2064383 res: 3582641 expected: 3609004
+	--		A: 2064383 B: 2618694 res: 3667269 expected: 3674602
+	--		A: 722076  B: 2416840 res: 2097152 expected: 2126960
+	--		A: 2064383 B: 873228  res: 1921803 expected: 1942471
 
     constant WORD_SIZE_TB : natural := 22;
     constant EXP_SIZE_TB  : natural := 6;
 
     signal a_aux : std_logic_vector(WORD_SIZE_TB - 1 downto 0)
-					:= std_logic_vector(to_unsigned(2064383, WORD_SIZE_TB));
+					:= std_logic_vector(to_unsigned(722076, WORD_SIZE_TB));
     signal b_aux : std_logic_vector(WORD_SIZE_TB - 1 downto 0)
-					:= std_logic_vector(to_unsigned(873228, WORD_SIZE_TB));
+					:= std_logic_vector(to_unsigned(2416840, WORD_SIZE_TB));
     signal s_aux : std_logic_vector(WORD_SIZE_TB - 1 downto 0);
 
 	signal expected : std_logic_vector(WORD_SIZE_TB - 1 downto 0)
-					:= std_logic_vector(to_unsigned(1942471, WORD_SIZE_TB));
+					:= std_logic_vector(to_unsigned(3609004, WORD_SIZE_TB));
 begin
 
     a_aux <= (others => '0') after 50 ns;
