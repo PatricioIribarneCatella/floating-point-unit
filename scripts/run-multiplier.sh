90d2df641d3cfe5e08408af9611407f064e26a6e
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-07-02 01:52:54 -0300

    Remove scripts and files not needed

--- scripts/run-multiplier.sh
+++ scripts/run-multiplier.sh
@@ -2,5 +2,16 @@
 
 set -eu
 
+FILE=$1
+
+./scripts/generate-tb.py $FILE
 ./scripts/compile-multiplier.sh
 ./scripts/simul-multiplier.sh
+
+#while [[ $# -gt 0 ]]; do
+#	file=$1
+#	./scripts/generate-tb.py $file
+#	./scripts/compile-multiplier.sh
+#	./scripts/simul-multiplier.sh $file
+#	shift
+#done
--- scripts/run-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-./scripts/compile-test.sh
-./scripts/simul-test.sh
